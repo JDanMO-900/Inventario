430376d2254e340f195129eb07883b7bdceb5d8d
Fabricio Corvera <ccorvera@cultura.gob.sv>
2024-07-05 16:03:52 -0600

    Cambios de diseño

--- frontend/src/views/reports/GeneralReportView.vue
+++ frontend/src/views/reports/GeneralReportView.vue
@@ -6,12 +6,12 @@
                 <v-row>
                     <v-col cols="12" lg="6" md="8" sm="12">
                         <BaseSelect label='Marca' :items="brands" item-title='name'
-                            v-model.trim="v$.editedItem.brand.$model" :rules="v$.editedItem.brand" clearable>
-                        </BaseSelect>
+                            v-model.trim="v$.editedItem.brand.$model" :rules="v$.editedItem.brand" />
+
                     </v-col>
                     <v-col cols="12" lg="3" md="4" sm="12">
-                        <base-button type="primary" title="Generar reporte" class="mt-4 mb-3" block @click="sendDataForReport" 
-                        :loading="isLoading"/>
+                        <base-button type="primary" title="Generar reporte" class="mt-4 mb-3" block
+                            @click="sendDataForReport" />
                     </v-col>
                 </v-row>
             </v-container>
@@ -19,7 +19,6 @@
     </div>
 </template>
 <script>
-import { ref } from "vue";
 import BaseSelect from "@/components/base-components/BaseSelect.vue";
 import BaseButton from "@/components/base-components/BaseButton.vue";
 import backendApi from "../../services/backendApi";
@@ -27,7 +26,6 @@ import { useVuelidate } from "@vuelidate/core";
 import { required } from "@/lang/i18n";
 
 
-const isLoading = ref(false);
 export default {
     components: { BaseSelect, BaseButton },
     setup() {
@@ -37,20 +35,23 @@ export default {
         return {
             title: 'Reporte general de inventario',
             brands: [
-               
+
             ],
             editedItem: {
-                brand:[],
+                id: '',
+                brand: '',
             },
             defaultItem: {
-                brand: [],
-            },
+                id: '',
+                brand: '',
+            }
         }
     },
     validations() {
         return {
             editedItem: {
-                brand: { required },
+                id: {},
+                brand: {},
             }
         }
     },
@@ -65,6 +66,7 @@ export default {
                 })
             ];
 
+
             const responses = await Promise.all(requests).catch((error) => {
                 alert.error("No fue posible obtener el registro.")
             });
@@ -72,13 +74,16 @@ export default {
             if (responses) {
                 this.brands = this.selectBrands(responses[0].data.data)
             }
+            console.log('brands: ', responses[0].data.data[0]['id']);
         },
         selectBrands(data) {
             let brandList = this.brands
             data.forEach(function (item) {
                 brandList.push(item)
             })
+            console.log('brandList: ', brandList);
             return brandList
+
         },
         async sendDataForReport() {
             this.v$.editedItem.$validate();
@@ -87,20 +92,21 @@ export default {
                 return;
             }
             try {
-                
-                // const reportData = await backendApi.get(`/reportgeneralpdf`, this.editedItem, {
+
+                // const reportData = await backendApi.post(`/reportgeneralpdf`, this.editedItem, {
                 //     // blob: This retrieve the data as binary as information
                 //     responseType: 'blob',
                 // });
-                // console.log(reportData);
+
                 // // This line tells the computer is a pdf and translate the binary information to get the url
-                // const report_data = new Blob([reportData.data], { type: 'application/pdf' })
+                // const report_data = new Blob([reportData.data], { type: 'application/pdf' });
                 // const url_report = window.URL.createObjectURL(report_data);
-                // window.open('http://localhost:8000/api/reportgeneralpdf/');
-                window.open('http://localhost:8000/api/reportgeneralpdf/');
+                // window.open(url_report);
+                window.open(`http://localhost:8000/api/reportgeneralpdf?brand=${this.editedItem.brand}`);
+                console.log('brand: ', this.editedItem);
 
                 // alert.success(endStatus.data.message);
-                isLoading.value = false;
+
             } catch (error) {
                 console.log(error);
             }
