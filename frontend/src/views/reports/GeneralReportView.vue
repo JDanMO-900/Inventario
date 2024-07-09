dc48af500c5a7d0d5310c1fa506b58383908cebd
Fabricio Corvera <ccorvera@cultura.gob.sv>
2024-07-09 15:28:53 -0600

    Report completed

--- frontend/src/views/reports/GeneralReportView.vue
+++ frontend/src/views/reports/GeneralReportView.vue
@@ -11,7 +11,7 @@
                     </v-col>
                     <v-col cols="12" lg="3" md="4" sm="12">
                         <base-button type="primary" title="Generar reporte" class="mt-4 mb-3" block
-                            @click="sendDataForReport" />
+                            @click="sendDataForReport()" :loading="isLoading" />
                     </v-col>
                 </v-row>
             </v-container>
@@ -19,11 +19,14 @@
     </div>
 </template>
 <script>
+import { ref } from "vue";
 import BaseSelect from "@/components/base-components/BaseSelect.vue";
 import BaseButton from "@/components/base-components/BaseButton.vue";
 import backendApi from "../../services/backendApi";
 import { useVuelidate } from "@vuelidate/core";
-import { required } from "@/lang/i18n";
+import useAlert from "@/composables/useAlert.js"
+
+const isLoading = ref(false);
 
 
 export default {
@@ -74,25 +77,25 @@ export default {
             if (responses) {
                 this.brands = this.selectBrands(responses[0].data.data)
             }
-            console.log('brands: ', responses[0].data.data[0]['id']);
         },
         selectBrands(data) {
             let brandList = this.brands
             data.forEach(function (item) {
                 brandList.push(item)
             })
-            console.log('brandList: ', brandList);
+
             return brandList
 
         },
         async sendDataForReport() {
+
             this.v$.editedItem.$validate();
             if (this.v$.editedItem.$invalid) {
                 alert.error("Campos obligatorios");
                 return;
             }
             try {
-
+                isLoading.value = true;
                 const reportData = await backendApi.post(`/reportgeneralpdf`, this.editedItem, {
                     // blob: This retrieve the data as binary as information
                     responseType: 'blob',
@@ -102,8 +105,9 @@ export default {
                 const report_data = new Blob([reportData.data], { type: 'application/pdf' });
                 const url_report = window.URL.createObjectURL(report_data);
                 window.open(url_report);
-
+                isLoading.value = false;
             } catch (error) {
+                isLoading.value = false;
                 console.log(error);
             }
 
